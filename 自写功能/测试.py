fdf4ac0c9b438e22ea0f160c5fad8553992c0506
Qing Yi <495017921@qq.com>
2024-06-26 16:31:25 +0800

    dot

--- 自写功能/测试.py
+++ 自写功能/测试.py
@@ -1,31 +1,13 @@
-from wxauto import WeChat
-import time
 
-# 实例化微信对象
-wx = WeChat()
+import pandas as pd
+from sqlalchemy import create_engine
 
-# 指定监听目标
-listen_list = [
-    'wxauto交流'
-]
-for i in listen_list:
-    wx.AddListenChat(who=i, savepic=True)  # 添加监听对象并且自动保存新消息图片
+CON = 'mysql://root:huanqlu0123@39.98.120.220:3306/spider?charset=utf8mb4'  # 阿里ECS，这里的编码很重要，要写为mb4
 
-# 持续监听消息，并且收到消息后回复“收到”
-wait = 10  # 设置10秒查看一次是否有新消息
-while True:
-    msgs = wx.GetListenMessage()
-    for chat in msgs:
-        one_msgs = msgs.get(chat)  # 获取消息内容
-        # ===================================================
-        # 处理消息逻辑
-        #
-        # 处理消息内容的逻辑每个人都不同，按自己想法写就好了，这里不写了
-        #
-        # ===================================================
+def write_to_sql(df):
+    con = create_engine(CON)
+    # df['说话者'] = df['说话者'].str.encode('utf8')  # 转换说话者列的编码
+    pd.io.sql.to_sql(df, name='wechat_msg', con=con, if_exists='append', index=False)  # 追加写入
 
-        # 回复收到
-        for msg in one_msgs:
-            if msg.type == 'friend':
-                chat.SendMsg('收到')  # 回复收到
-    time.sleep(wait)
+df = pd.read_excel('本次获取内容.xlsx')
+write_to_sql(df)
