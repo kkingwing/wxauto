9d64e56a8d5ad22e4e5171cc00dde48eaa34feaa
Qing Yi <495017921@qq.com>
2024-09-29 18:16:49 +0800

    dot

---
+++ 小功能/rainbow_talk_v0.1.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# v0.2 清洗文本，加入ods、dw逻辑
+# v0.1 基本功能实现
+
+"""
+彩虹屁
+"""
+import random
+
+from 彩虹屁.语料库 import girl_ls
+from wxauto import WeChat
+from datetime import datetime
+import time
+import pandas as pd
+from sqlalchemy import create_engine
+from config import CON
+import re
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+wx = WeChat()
+
+
+# 一、功能： 聊天消息记录， 自动处理，调用excel宏生成分析图
+
+# wx.ChatWith("wxauto交流")
+# g1 = wx.CurrentChat() # 获取当前窗口名
+
+def df2sql(df, save_type, save_name):
+    """
+    保存到数据库，根据指定的存储类型 ods|dw
+
+    Args:
+        df(DataFrame): 数据
+        save_type(str): ods/dw
+        save_name(str): 表名
+    """
+    engine = create_engine(CON)
+    if save_type == "ods":
+        df.to_sql(rf'ods_{save_name}', con=engine, if_exists='append', index=False)
+    else:
+        print("请输入正确的保存类型")
+
+
+def rainbow_group(group):
+    """
+    监听群消息，并保存到数据库
+
+    Args:
+        group(str): 群名
+    """
+    wx.AddListenChat(who=group, savepic=False)  # 添加监听对象并且自动保存新消息图片
+    wait = 3  # 设置20秒查看一次是否有新消息
+    Me = "清易"
+    CACHE_NUMBER = 5  # 这是「缓存的消息条数」超过这个数就将缓存写到数据库中.
+    ls = []
+    while True:
+        msgs = wx.GetListenMessage()  # 这一句会先取原有的聊天内容，根据上下的聊天内容判断消耗新id是否为新
+        for chat in msgs:  # chat 是一个带有聊天名称的 uia提示
+            one_msgs = msgs.get(chat)  # 获取消息内容,one_msgs[0] # 说话者/时间/系统提示  one_msgs[1] 说话内容
+            for msg in one_msgs:
+                ds = {}
+                ds['记录日期'] = datetime.now().strftime("%Y-%m-%d")
+                ds['群名'] = chat  # listen_list[0]  # 暂时用这个方法
+                ds['内容类型'] = msg.type  # 需要清洗处理时另外写，这里取原始数据
+                ds['发言者'] = Me if msg[0] == "Self" else msg[0]  # 如果是自己说话，使用名称替换
+                ds['发言内容'] = msg[1]
+                ds['发言时间'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 误差约有{wait}秒
+                # chat.SendMsg('收到')  # 回复收到
+                if '#夸我' in ds['发言内容'] and ds['发言者'] != 'SYS':
+                    # @某人并发送消息
+                    for _ in range(3):  # 发送3条
+                        msg = ' ' + random.choice(girl_ls)  # 语料库随机
+                        at = [ds['发言者'], ]  # at = ['数据运营', '墨云霄']  # 要@的人
+                        who = group  # 群聊
+                        wx.SendMsg(msg=msg, who=who, at=at)
+                        ds['夸奖内容'] = msg
+                        print(ds)
+                        ls.append(ds)
+                # 写入数据库
+                if len(ls) >= CACHE_NUMBER:  # 这是「缓存的消息条数」超过这个数就将缓存写到数据库中.
+                    df = pd.DataFrame(ls)
+                    print(f'缓存消息大于 {CACHE_NUMBER} 条,df写入sql：\n{df}\n')
+                    df2sql(df, save_type="ods", save_name="wxauto_wechat_rainbow")
+                    ls = []  # 重置列表为
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{now} 缓存条数：{len(ls)}")
+        time.sleep(wait)
+
+
+# 免打扰也可获得内容
+group = '测试群wxauto'  # "基金梦工厂", "wxauto三群" # "天麒的高端会所群",
+rainbow_group(group)
